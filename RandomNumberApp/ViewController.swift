233e92753cb72a56ead7c4bbf6b0763f9c571b7c
Denis Gindulin <denisgindulin@me.com>
2023-06-07 13:12:53 +0300

    Initial Commit

---
+++ RandomNumberApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RandomNumberApp
+//
+//  Created by Денис Гиндулин on 07.06.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
