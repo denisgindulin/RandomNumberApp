6796b795ca08d2938655cf2161b5bad66719ed28
Denis Gindulin <denisgindulin@me.com>
2023-06-07 16:37:34 +0300

    added SettingsViewController-file

---
+++ RandomNumberApp/SettingsViewController.swift
@@ -0,0 +1,16 @@
+//
+//  SettingsViewController.swift
+//  RandomNumberApp
+//
+//  Created by Денис Гиндулин on 07.06.2023.
+//
+
+import UIKit
+
+final class SettingsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
